6307849581e1a1725e8ff6218840f38de49a66d5
Eugene1311 <ef7@mail.ru>
2015-02-14 14:40:52 +0300

    1

--- js/main.js
+++ js/main.js
@@ -3,5 +3,19 @@ var Person = Backbone.Model.extend({
 		name: 'Eugene',
 		age: 36,
 		job: 'undefined'
+	},
+	validate: function(attrs) {
+		if( attrs.age <= 0){
+			return 'Возраст должен быть положительным!';
+		}
 	}
 });
+var PersonView = Backbone.View.extend({ 
+	
+});
+var personView = new PersonView;
+var person = new Person({'name': 'Евгений'});
+//person.save({age:-5},{validate:true});
+//person.on("invalid", function(model, error) {
+//  console.log(model.get("age") + " " + error);
+//});
